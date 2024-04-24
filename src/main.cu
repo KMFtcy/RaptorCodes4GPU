a5b622bee18a239f0be75f7ede811beffff857d8
kmftang <kmftangchaoyang@gmail.com>
2024-04-24 22:28:20 +0800

    add allocate program in main

--- src/main.cu
+++ src/main.cu
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <main.hpp>
 
 using namespace std;
 
 int main(){
     cout<< "Hello, R11 for GPU!" << endl;
+
+    const int N = 100000000;
+    const int M = sizeof(float)*N;
+
+    //allocate memory for data
+    float *d_x;
+    cudaMalloc((void **)&d_x,M);
+    cout << "Allocate " << M << "Bytes" << endl;
+
+    //free memory
+    cudaFree(d_x);
 }
